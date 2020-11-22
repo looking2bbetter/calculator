48b2a495d766baa770289af9a2baab310de2ac0a
looking2bbetter <jtburroughs1998@yahoo.com>
2020-11-22 13:27:26 -0800

    Added some functionality to change the display when a number button is pressed

---
+++ script.js
@@ -0,0 +1,53 @@
+function add () {
+
+}
+
+function subtract () {
+
+}
+
+function multiply () {
+
+}
+
+function divide () {
+
+}
+
+function digitDisplay () {
+    // addEventListener forEach digit button that displays the display value in a variable.
+
+    let digits = document.querySelector('.digit');
+    let display = document.querySelector('.display');
+
+    console.log("I'm above the forEach function");
+
+    // digits.forEach(function(digit) {
+    //     digit.addEventListener('click', function () {
+    //         display.innerText = digits.innerText;
+    //         console.log("I'm in the forEach function");
+    //     })
+    // })
+    
+
+    // digits.forEach(function(digit) {
+    //     digit.addEventListener('click', console.log('click'))
+    // });
+}
+
+const digitButtons = Array.from(document.querySelectorAll('.digits > div > .numb'));
+
+function itWorked () {
+    console.log('this pisses me off');
+}
+
+function numPressed () {
+    document.querySelector('.display > p').textContent = 'testing';
+}
+
+digitButtons.forEach(digit => digit.addEventListener('click', numPressed));
+
+document.querySelector('.button').addEventListener('click', console.log('april'));
+
+document.querySelectorAll('.digits').forEach(function(digit) {
+    digit.addEventListener('click', console.log('yooo'))});
