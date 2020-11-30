e8a3463a64514ac20319c634e7a264d34354f715
looking2bbetter <jtburroughs1998@yahoo.com>
2020-11-29 17:50:05 -0800

    added functionality for the addition button... it works! but it will need reworking.

--- script.js
+++ script.js
@@ -1,53 +1,35 @@
+let a, b;
+
 function add () {
+    a = getDisplay() * 1;
 
+    clear();
 }
 
-function subtract () {
-
+function equal () {
+    b = getDisplay() * 1;
+    return displayText.textContent = a + b;
 }
 
-function multiply () {
-
+function clear () {
+    displayText.textContent = '';
 }
 
-function divide () {
+document.querySelector('.clear').addEventListener('click', clear);
 
-}
-
-function digitDisplay () {
-    // addEventListener forEach digit button that displays the display value in a variable.
-
-    let digits = document.querySelector('.digit');
-    let display = document.querySelector('.display');
-
-    console.log("I'm above the forEach function");
-
-    // digits.forEach(function(digit) {
-    //     digit.addEventListener('click', function () {
-    //         display.innerText = digits.innerText;
-    //         console.log("I'm in the forEach function");
-    //     })
-    // })
-    
-
-    // digits.forEach(function(digit) {
-    //     digit.addEventListener('click', console.log('click'))
-    // });
-}
-
-const digitButtons = Array.from(document.querySelectorAll('.digits > div > .numb'));
-
-function itWorked () {
-    console.log('this pisses me off');
-}
-
+const digitButtons = Array.from(document.querySelectorAll('.digits > div > .number'));
+let displayText = document.querySelector('.display > p');
 function numPressed (e) {
-    document.querySelector('.display > p').textContent = e.target.textContent;
+    displayText.textContent += e.target.textContent;
 }
 
 digitButtons.forEach(digit => digit.addEventListener('click', numPressed));
 
-document.querySelector('.button').addEventListener('click', console.log('april'));
+function getDisplay () {
+    return displayText.textContent;    
+}
 
-document.querySelectorAll('.digits').forEach(function(digit) {
-    digit.addEventListener('click', console.log('yooo'))});
+let addButton = document.querySelector('.add');
+addButton.addEventListener('click', add);
+let equalButton = document.querySelector('.equal');
+equalButton.addEventListener('click', equal);
